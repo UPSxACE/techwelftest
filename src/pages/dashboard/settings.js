d75d23ba6d22f527f6ad4da5fced4bea2df3f758
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2023-03-09 16:16:52 +0000

    reorganizing code

--- src/pages/dashboard/settings.js
+++ src/pages/dashboard/settings.js
@@ -1,12 +1,76 @@
 import DashboardWrapper from '@/components/dashboard-wrapper';
-import CompanySettings from '@/components/pages/settings/company-settings';
 import DashboardLayout from '@/layouts/dashboard-layout';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import DashboardPageHeader from '@/components/dashboard-page-header';
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Box } from '@mui/material';
+import Joi from 'joi';
+import AccordionForm from '@/components/accordion-form';
+import CompanySettingsForm from '@/components/pages/settings/forms/companysettingsform';
+import UsersTable from '@/components/pages/settings/tables/userstable';
+import RolesTable from '@/components/pages/settings/tables/rolestable';
 
 export default function Settings() {
+  const [formData, setFormData] = useState({});
+  const { t } = useTranslation();
+
+  const defaultValues = {
+    companyId: '000',
+    companyName: 'Test',
+    email: 'data_from@backend.com',
+  };
+
+  const validators = {
+    email: Joi.string().email({ tlds: { allow: false } }),
+    newPassword: Joi.string().min(9),
+    newPasswordConfirm: Joi.string(),
+  };
+
+  useEffect(() => {
+    console.log(formData);
+  }, [formData]);
+
   return (
     <DashboardWrapper notAligned>
-      <CompanySettings />
+      <Box
+        sx={{
+          flex: 1,
+          flexDirection: 'column',
+          display: 'flex',
+        }}
+      >
+        <DashboardPageHeader
+          title='Settings'
+          rightText={'Dashboard / Settings'}
+        />
+        <Box
+          sx={{
+            flex: 1,
+            backgroundColor: 'white',
+            border: '1px solid #dadce0',
+            borderRadius: 1,
+            boxShadow:
+              '0 0 5px 0 rgb(43 43 43 / 10%), 0 11px 6px -7px rgb(43 43 43 / 10%)',
+            overflow: 'hidden',
+            //padding: 5,
+          }}
+        >
+          <AccordionForm.Form>
+            <AccordionForm.Part title={t('CompanySettings')} id={1}>
+              <CompanySettingsForm />
+            </AccordionForm.Part>
+            <AccordionForm.Part title={t('SettingsUsers')} id={2}>
+              <UsersTable />
+            </AccordionForm.Part>
+            <AccordionForm.Part title={t('SettingsRoles')} id={3}>
+              <RolesTable />
+            </AccordionForm.Part>
+            <AccordionForm.Part title={t('SettingsInvoicing')} id={4} />
+            <AccordionForm.Part title={t('AdvancedSettings')} id={5} />
+          </AccordionForm.Form>
+        </Box>
+      </Box>
     </DashboardWrapper>
   );
 }
