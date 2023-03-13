9a673628b0c302c09eeb0ae7a08157c168eca13d
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2023-03-13 16:42:13 +0000

    additional step to confirming email: creating password

--- src/pages/confirmation.js
+++ src/pages/confirmation.js
@@ -12,9 +12,11 @@ import axios from 'axios';
 
 function Confirmation() {
   const [formData, setFormData] = useState({});
+  const [formData2, setFormData2] = useState({});
   const { query, isReady } = useRouter();
   const { token } = query;
   const [ready, setReady] = useState(false);
+  const [tokenConfirmed, setTokenConfirmed] = useState(null); // false or string with the confirmed token
 
   const { t } = useTranslation();
 
@@ -24,6 +26,11 @@ function Confirmation() {
 
   const validators = {
     cToken: Joi.string(),
+  };
+
+  const validators2 = {
+    newPassword: Joi.string().min(9),
+    newPasswordConfirm: Joi.string(),
   };
 
   useEffect(() => {
@@ -42,11 +49,10 @@ function Confirmation() {
         padding: { xs: 2, sm: 8, md: 14 },
       }}
     >
-      {ready ? (
+      {ready && tokenConfirmed === null ? (
         <OutlinedForm.Form
           defaultValues={defaultValues}
           formDataState={{ formData, setFormData }}
-          autoFinalize
         >
           <OutlinedForm.Header>
             <Box sx={{ mb: 2 }}>
@@ -86,13 +92,85 @@ function Confirmation() {
                 }
               );
             }}
+            onSuccess={async (result) => {
+              setTokenConfirmed(true);
+            }}
             onError={async (error, setStatus) => {
               setStatus(error);
             }}
           />
         </OutlinedForm.Form>
       ) : (
-        <LoaderPrimary />
+        tokenConfirmed === null && <LoaderPrimary />
+      )}
+      {(tokenConfirmed === true || tokenConfirmed === false) && (
+        <OutlinedForm.Form
+          formDataState={{ formData: formData2, setFormData: setFormData2 }}
+          autoFinalize
+        >
+          <OutlinedForm.Header>
+            <Box sx={{ mb: 2 }}>
+              <Typography
+                sx={{ textAlign: 'center', mb: 2 }}
+                variant='h3'
+                component='h1'
+              >
+                {t('Confirmation_newPass')}
+              </Typography>
+              <Box>
+                <Typography variant='body1'>
+                  {t('Confirmation_newPass_text')}
+                </Typography>
+              </Box>
+            </Box>
+          </OutlinedForm.Header>
+          <OutlinedForm.Control
+            required
+            label={t('Confirmation_newPass_input')}
+            field='newPassword'
+          >
+            <OutlinedForm.Label />
+            <OutlinedForm.Input
+              JOIValidator={validators2.newPassword}
+              tooltip={{
+                tip: t('tooltip_tip_password'),
+                example: 'VerySafeP4ssw0rd##',
+              }}
+              inputProps={{ type: 'password' }}
+            />
+            <OutlinedForm.HelperText />
+          </OutlinedForm.Control>
+          <OutlinedForm.Control
+            required
+            label={t('Confirmation_confirmNewPass_input')}
+            field='newPasswordConfirm'
+            matchesPassword={'newPassword'}
+          >
+            <OutlinedForm.Label />
+            <OutlinedForm.Input
+              JOIValidator={validators2.newPasswordConfirm}
+              inputProps={{ type: 'password' }}
+            />
+            <OutlinedForm.HelperText />
+          </OutlinedForm.Control>
+          <OutlinedForm.Submit
+            title='submit'
+            validators={validators2}
+            onSubmit={async (formData) => {
+              // Test endpoint
+              await axios.post(
+                'http://localhost:9000/test/formdata',
+                formData,
+                {
+                  headers: { 'Content-Type': 'multipart/form-data' },
+                }
+              );
+            }}
+            onError={async (error, setStatus) => {
+              setStatus(error);
+            }}
+          />
+        </OutlinedForm.Form>
       )}
     </Box>
   );
